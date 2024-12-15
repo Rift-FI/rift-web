447f5f8129bc7921e6bbc1dd13346ba3bd04c053
mrgustaveh <mburuantony@outlook.com>
2024-12-15 17:16:11 +0300

    updates & fixes

--- src/utils/ethusd.ts
+++ src/utils/ethusd.ts
@@ -8,9 +8,15 @@ export const getEthUsdVal = async (
   ethVal?: number
 ): Promise<{ ethInUSD: number; success: boolean }> => {
   const APIURL =
-    "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd";
+    "https://pro-api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd&x_cg_pro_api_key=CG-Whw1meTdSTTT7CSpGZbaB3Yi";
 
-  let res: Response = await fetch(APIURL, { method: "GET" });
+  let res: Response = await fetch(APIURL, {
+    method: "GET",
+
+    headers: {
+      accept: "application/json",
+    },
+  });
   let data: ethUSD = await res.json();
   let ethToUsd: number = data?.ethereum?.usd;
 
