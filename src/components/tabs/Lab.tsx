ac5997ca2fdd4a002e2c98b0d1da2c7eab698ee3
mrgustaveh <mburuantony@outlook.com>
2024-12-19 15:12:40 +0300

    created labs tab

---
+++ src/components/tabs/Lab.tsx
@@ -0,0 +1,40 @@
+import { JSX } from "react";
+import friendsduel from "../../assets/images/labs/friendsduel.png";
+import telemarket from "../../assets/images/labs/telemarket.png";
+import { colors } from "../../constants";
+import { ComingSoon } from "../../assets/icons";
+import "../../styles/components/tabs/labstab.css";
+
+export const LabsTab = (): JSX.Element => {
+  return (
+    <section id="labstab">
+      <p className="title">Labs</p>
+
+      <div className="projects">
+        <div className="project">
+          <img src={friendsduel} alt="friendsduel" />
+
+          <div className="about">
+            <p>
+              FriendsDuel
+              <ComingSoon width={16} height={18} color={colors.textsecondary} />
+            </p>
+            <span>Create groups, send & share crypto...</span>
+          </div>
+        </div>
+
+        <div className="project">
+          <img src={telemarket} alt="friendsduel" />
+
+          <div className="about">
+            <p>
+              Telemarket
+              <ComingSoon width={16} height={18} color={colors.textsecondary} />
+            </p>
+            <span>Modern crypto trading</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
