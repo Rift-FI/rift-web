6e659c2896d0f238448f6a3f2cabce39569a5604
mrgustaveh <mburuantony@outlook.com>
2024-12-22 17:51:08 +0300

    updates & fixes - btc -usd -shared secrets

---
+++ src/pages/EthAsset.tsx
@@ -0,0 +1,95 @@
+import { JSX, useCallback, useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import { backButton } from "@telegram-apps/sdk-react";
+import { AppDrawer } from "../components/global/AppDrawer";
+import { useSnackbar } from "../hooks/snackbar";
+import { useAppDrawer } from "../hooks/drawer";
+import { getEthUsdVal } from "../utils/ethusd";
+import { walletBalance } from "../utils/api/wallet";
+import { formatUsd } from "../utils/formatters";
+import { Copy, Send, Receive } from "../assets/icons";
+import { colors } from "../constants";
+import ethlogo from "../assets/images/eth.png";
+import "../styles/pages/assets.css";
+
+export default function EthAsset(): JSX.Element {
+  const navigate = useNavigate();
+  const { showsuccesssnack } = useSnackbar();
+  const { openAppDrawer, drawerOpen } = useAppDrawer();
+
+  const [accBalLoading, setAccBalLoading] = useState<boolean>(false);
+  const [accBalance, setAccBalance] = useState<number>(0);
+  const [amountInUsd, setAmountInUsd] = useState<number>(0);
+
+  if (backButton.isMounted()) {
+    backButton.onClick(() => {
+      navigate(-1);
+    });
+  }
+
+  let walletAddress = localStorage.getItem("address");
+
+  const onCopyAddr = () => {
+    if (walletAddress !== null) {
+      navigator.clipboard.writeText(walletAddress as string);
+      showsuccesssnack("BTC address copied to clipboard");
+    }
+  };
+
+  const onGetBalance = useCallback(async () => {
+    setAccBalLoading(true);
+
+    let access: string | null = localStorage.getItem("token");
+
+    const { balance } = await walletBalance(access as string);
+    const { ethInUSD } = await getEthUsdVal(Number(balance));
+
+    setAccBalance(Number(balance));
+    setAmountInUsd(ethInUSD);
+
+    setAccBalLoading(false);
+  }, []);
+
+  useEffect(() => {
+    if (backButton.isSupported()) {
+      backButton.mount();
+      backButton.show();
+    }
+
+    return () => {
+      backButton.unmount();
+    };
+  }, [drawerOpen]);
+
+  useEffect(() => {
+    onGetBalance();
+  }, []);
+
+  return (
+    <section id="eth-asset">
+      <img src={ethlogo} alt="eth" />
+
+      <button className="address" onClick={onCopyAddr}>
+        {walletAddress?.substring(0, 3)}...{walletAddress?.substring(4, 7)}
+        <Copy width={14} height={16} color={colors.textsecondary} />
+      </button>
+
+      <div className="balance">
+        <p>{accBalLoading ? "- - -" : `${formatUsd(amountInUsd)}`}</p>
+        <span>{accBalLoading ? "- - -" : `${accBalance?.toFixed(8)} ETH`}</span>
+      </div>
+
+      <div className="actions">
+        <button className="send" onClick={() => openAppDrawer("sendoptions")}>
+          Send ETH <Send width={18} height={18} color={colors.textprimary} />
+        </button>
+        <button className="receive">
+          Receive ETH
+          <Receive width={18} height={18} color={colors.textprimary} />
+        </button>
+      </div>
+
+      <AppDrawer />
+    </section>
+  );
+}
