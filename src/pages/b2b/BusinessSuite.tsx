1acd619b053510afab7d061bb1a630fefb28f05a
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2025-01-27 11:04:18 +0300

    update: enhance Business Suite page with additional icons and improved layout

--- src/pages/b2b/BusinessSuite.tsx
+++ src/pages/b2b/BusinessSuite.tsx
@@ -1,22 +1,32 @@
 import { useEffect } from "react";
 import toast from "react-hot-toast";
 import { Link } from "react-router";
-import { IconArrowBackUp } from "@tabler/icons-react";
+import {
+  IconArrowBackUp,
+  IconChartInfographic,
+  IconHomeFilled,
+  IconUserCircle,
+} from "@tabler/icons-react";
 
 function BusinessSuite() {
   toast.success("Switched to Sphere Business");
   useEffect(() => {}, []);
   return (
-    <div className="h-screen bg-primary font-body  text-center">
+    <div className="h-screen bg-primary font-body relative">
       <Link to="/app">
         <IconArrowBackUp size={26} />
       </Link>
-      <h1 className="text-textprimary text-xl font-bold mt-2">
+      <h1 className="text-textprimary text-xl font-bold text-center">
         Business One Click Suite
       </h1>
-      <p className="text-sm text-textsecondary text-s leading-relaxed pb-4 border-b-[1px] border-gray-500">
+      <p className="text-sm text-textsecondary text-center leading-relaxed pb-2 border-b-[1px] border-gray-500">
         Streamline Airdrops and Token Distributions with One Click
       </p>
+      <div className="absolute bottom-4 w-full px-4 items-center justify-between flex bg-primary/50">
+        <IconHomeFilled size={26} />
+        <IconChartInfographic size={26} />
+        <IconUserCircle size={26} />
+      </div>
     </div>
   );
 }
