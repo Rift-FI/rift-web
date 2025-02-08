60bdebd4c9a6d40190fdde7dd35968a63707057f
amschel99 <6290.2020@students.ku.ac.ke>
2025-02-08 17:17:03 +0800

    update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -86,6 +86,7 @@ createRoot(document.getElementById("root")!).render(
                   />
                   <Route path="/importsecret" element={<ImportSecret />} />
                   <Route path="/importawx" element={<ImportAirwllxKey />} />
+                  
                   <Route
                     path="/sharesecret/:key/:purpose"
                     element={<ShareSecret />}
