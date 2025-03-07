1c26ca1610a33ca45a2f7bc5dacc38c5f3272b4f
mrgustaveh <mburuantony@outlook.com>
2025-03-07 16:40:17 +0300

    setup quvault endpoints & methods

---
+++ src/utils/api/quvault/launchpad.ts
@@ -0,0 +1,43 @@
+import { QUVAULT_BASEURL, QUVAULT_ENDPOINTS } from "../config";
+
+export type launchproject = {
+  store_id: string;
+  store_name: string;
+  merchant_email: string;
+  id: string;
+  symbol: string;
+  logo_url: string;
+  blockchain: number;
+  price: number;
+  total_supply: number;
+  presale_percentage: number;
+  liquidity_percentage: number;
+  merchant_percentage: number;
+  margin_percentage: number;
+  offered_at: string;
+  apy: number;
+  unlocking_condition: number;
+  reversal_condition: number;
+  token_address: string | null;
+  contract_address: string | null;
+  status: string;
+  created_at: string;
+  updated_at: string;
+};
+
+export const getLaunchPadProjects = async (): Promise<{
+  data: launchproject[];
+}> => {
+  let URL = QUVAULT_BASEURL + QUVAULT_ENDPOINTS.launchpad;
+  let quvaultToken = localStorage.getItem("quvaulttoken");
+
+  const res = await fetch(URL, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${quvaultToken}`,
+    },
+  });
+
+  return res.json();
+};
