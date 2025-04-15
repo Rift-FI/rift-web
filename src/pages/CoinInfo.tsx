1a55607390033a13ed1b79cc10fbdece3fed8bb1
mrgustaveh <mburuantony@outlook.com>
2025-04-15 16:50:08 +0300

    added sphere usdc exchange rate

--- src/pages/CoinInfo.tsx
+++ src/pages/CoinInfo.tsx
@@ -1,44 +1,32 @@
-import { JSX, Fragment, useState } from "react";
-import { useQueryClient, useQuery } from "@tanstack/react-query";
-import { openLink } from "@telegram-apps/sdk-react";
-import { CandlestickData } from "lightweight-charts";
-import { useParams, useNavigate } from "react-router";
-import { useBackButton } from "../hooks/backbutton";
-import { useSnackbar } from "../hooks/snackbar";
-import { useTabs } from "../hooks/tabs";
-import { coinInfoType } from "../types/earn";
-import { fetchCoinPrices, fetchCoinInfo } from "../utils/api/market";
-import { numberFormat, formatUsd } from "../utils/formatters";
-import { SubmitButton } from "../components/global/Buttons";
-import { CoinPriceChart } from "../components/PriceChart";
-import { colors } from "../constants";
+import { JSX } from "react";
+import { useNavigate } from "react-router";
+import { useTabs } from "@/hooks/tabs";
+import { useBackButton } from "@/hooks/backbutton";
+import { CoinPriceChart } from "@/components/PriceChart";
+import { coinPriceType } from "@/types/earn";
+import { useQuery } from "@tanstack/react-query";
+import { getSphrUsdcRate } from "@/utils/api/mantra";
 import "../styles/pages/coininfo.scss";
 
 export default function CoinInfo(): JSX.Element {
-  const { coinId } = useParams();
   const navigate = useNavigate();
   const { switchtab } = useTabs();
-  const queryclient = useQueryClient();
-  const { showerrorsnack } = useSnackbar();
 
-  const [dayCountPrices, setDaycountPrices] = useState<number>(30);
+  const prevpage = localStorage.getItem("prev_page");
 
-  const { data: coininfoDetails } = useQuery({
-    queryKey: ["coindetails"],
-    queryFn: () => fetchCoinInfo(coinId as string),
-    refetchInterval: 3000,
+  const { data: sphrUsdcRateData, isLoading: sphrUsdcRateLoading } = useQuery({
+    queryKey: ["sphrUsdcRate"],
+    queryFn: getSphrUsdcRate,
   });
-
-  const { data: coininfoPrices } = useQuery({
-    queryKey: ["coinprices"],
-    queryFn: () => fetchCoinPrices(coinId as string, dayCountPrices),
-    refetchInterval: 3000,
-  });
-  const coinDetails = coininfoDetails as coinInfoType;
-  const coinPrices = coininfoPrices as CandlestickData[];
 
   const goBack = () => {
-    switchtab("earn");
+    if (prevpage == null) {
+      switchtab("home");
+    } else {
+      switchtab("rewards");
+    }
+
+    localStorage.removeItem("prev_page");
     navigate("/app");
   };
 
@@ -46,186 +34,58 @@ export default function CoinInfo(): JSX.Element {
 
   return (
     <section id="aboutcoin">
-      <Fragment>
-        <div id="loo1">
-          <p className="name_symbol">
-            {coinDetails && coinDetails?.name}{" "}
-            <span>{coinDetails && coinDetails?.symbol}</span>
-          </p>
-          <p className="price">
-            {formatUsd(
-              coinDetails && coinDetails?.market_data?.current_price?.usd
-            )}
-          </p>
-          <p
-            className="price_change24"
-            style={{
-              fontStyle: "normal",
-              fontWeight: "600",
-              color:
-                coinDetails &&
-                coinDetails?.market_data?.price_change_percentage_24h < 0
-                  ? colors.danger
-                  : colors.success,
-            }}
-          >
-            {coinDetails &&
-            coinDetails?.market_data?.price_change_percentage_24h > 0
-              ? `+${
-                  coinDetails &&
-                  coinDetails?.market_data?.price_change_percentage_24h.toFixed(
-                    2
-                  )
-                }%`
-              : `${
-                  coinDetails &&
-                  coinDetails?.market_data?.price_change_percentage_24h.toFixed(
-                    2
-                  )
-                }%`}
-          </p>
-        </div>
+      <p className="infoctr">
+        SPHR / USDC Exchange Rate
+        <span>Rates for the last 30 days</span>
+      </p>
 
-        <div className="prices">
-          <CoinPriceChart data={coinPrices || []} />
+      <div className="chartctr">
+        <CoinPriceChart data={last30DaysData} />
+      </div>
 
-          <div className="dayscount">
-            <button
-              className={dayCountPrices == 1 ? "selecteddays" : ""}
-              onClick={() => {
-                setDaycountPrices(1);
-                queryclient.invalidateQueries({ queryKey: ["coinprices"] });
-              }}
-            >
-              1D
-            </button>
-            <button
-              className={dayCountPrices == 7 ? "selecteddays" : ""}
-              onClick={() => {
-                setDaycountPrices(7);
-                queryclient.invalidateQueries({ queryKey: ["coinprices"] });
-              }}
-            >
-              1W
-            </button>
-            <button
-              className={dayCountPrices == 30 ? "selecteddays" : ""}
-              onClick={() => {
-                setDaycountPrices(30);
-                queryclient.invalidateQueries({ queryKey: ["coinprices"] });
-              }}
-            >
-              1M
-            </button>
-            <button
-              className={dayCountPrices == 365 ? "selecteddays" : ""}
-              onClick={() => {
-                setDaycountPrices(365);
-                queryclient.invalidateQueries({ queryKey: ["coinprices"] });
-              }}
-            >
-              1Y
-            </button>
-          </div>
-        </div>
-
-        <div id="loo3">
-          <p>
-            Rank <span>NO.{coinDetails?.market_cap_rank}</span>
-          </p>
-          <p>
-            Market Cap
-            <span>
-              ${numberFormat(Number(coinDetails?.market_data?.market_cap?.usd))}
-            </span>
-          </p>
-          <p>
-            Circulation Supply
-            <span>
-              {numberFormat(
-                Number(coinDetails?.market_data?.circulating_supply)
-              )}
-              &nbsp;
-              <em
-                className="sym"
-                style={{ textTransform: "uppercase", fontStyle: "normal" }}
-              >
-                {coinDetails?.symbol}
-              </em>
-            </span>
-          </p>
-          <p>
-            Max Supply
-            <span>
-              {typeof coinDetails?.market_data?.max_supply == "number"
-                ? numberFormat(Number(coinDetails?.market_data?.max_supply))
-                : "---"}
-            </span>
-          </p>
-          <p>
-            Total Supply
-            <span>
-              {numberFormat(Number(coinDetails?.market_data?.total_supply))}
-              &nbsp;
-              <em
-                className="sym"
-                style={{ textTransform: "uppercase", fontStyle: "normal" }}
-              >
-                {coinDetails?.symbol}
-              </em>
-            </span>
-          </p>
-          <p>
-            Issue Date <span>{coinDetails?.genesis_date || "---"}</span>
-          </p>
-        </div>
-
-        <p id="loo2">{coinDetails && coinDetails?.description?.en}</p>
-
-        <div id="loo4">
-          <span
-            onClick={() =>
-              openLink(coinDetails && coinDetails?.links?.homepage[0])
-            }
-          >
-            Official Website
-          </span>
-          <span
-            onClick={() =>
-              openLink(coinDetails && coinDetails?.links?.whitepaper)
-            }
-          >
-            Whitepaper
-          </span>
-        </div>
-
-        <div id="loo5">
-          {coinDetails &&
-            coinDetails?.categories?.map((_cat, idx) => (
-              <span key={_cat + idx}>{_cat}</span>
-            ))}
-        </div>
-      </Fragment>
-
-      <div className="buy_sell">
-        <SubmitButton
-          text="Buy"
-          onclick={() => showerrorsnack("Feature coming soon...")}
-          sxstyles={{
-            width: "48%",
-            borderRadius: "1rem",
-          }}
-        />
-
-        <SubmitButton
-          text="Sell"
-          onclick={() => showerrorsnack("Feature coming soon...")}
-          sxstyles={{
-            width: "48%",
-            borderRadius: "1rem",
-          }}
-        />
+      <div className="currenrate">
+        <p>Current Exchange Rate</p>
+        <span>
+          1 SPHR ≈{" "}
+          {sphrUsdcRateLoading
+            ? 0
+            : sphrUsdcRateData?.data?.currentRate?.substring(0, 5)}
+          &nbsp;USDC
+        </span>
       </div>
     </section>
   );
 }
+
+const last30DaysData: coinPriceType[] = [
+  { time: "2025-03-17", open: 0.128, high: 0.135, low: 0.128, close: 0.123 },
+  { time: "2025-03-18", open: 0.123, high: 0.132, low: 0.117, close: 0.114 },
+  { time: "2025-03-19", open: 0.114, high: 0.122, low: 0.101, close: 0.108 },
+  { time: "2025-03-20", open: 0.108, high: 0.109, low: 0.095, close: 0.103 },
+  { time: "2025-03-21", open: 0.103, high: 0.112, low: 0.09, close: 0.101 },
+  { time: "2025-03-22", open: 0.101, high: 0.109, low: 0.097, close: 0.1 },
+  { time: "2025-03-23", open: 0.1, high: 0.106, low: 0.091, close: 0.099 },
+  { time: "2025-03-24", open: 0.099, high: 0.106, low: 0.086, close: 0.099 },
+  { time: "2025-03-25", open: 0.099, high: 0.102, low: 0.094, close: 0.099 },
+  { time: "2025-03-26", open: 0.099, high: 0.1, low: 0.087, close: 0.099 },
+  { time: "2025-03-27", open: 0.099, high: 0.111, low: 0.084, close: 0.099 },
+  { time: "2025-03-28", open: 0.099, high: 0.113, low: 0.099, close: 0.099 },
+  { time: "2025-03-29", open: 0.099, high: 0.099, low: 0.092, close: 0.099 },
+  { time: "2025-03-30", open: 0.099, high: 0.107, low: 0.091, close: 0.099 },
+  { time: "2025-03-31", open: 0.099, high: 0.106, low: 0.092, close: 0.099 },
+  { time: "2025-04-01", open: 0.099, high: 0.104, low: 0.094, close: 0.099 },
+  { time: "2025-04-02", open: 0.099, high: 0.103, low: 0.093, close: 0.099 },
+  { time: "2025-04-03", open: 0.099, high: 0.112, low: 0.092, close: 0.099 },
+  { time: "2025-04-04", open: 0.099, high: 0.105, low: 0.086, close: 0.099 },
+  { time: "2025-04-05", open: 0.099, high: 0.111, low: 0.086, close: 0.099 },
+  { time: "2025-04-06", open: 0.099, high: 0.108, low: 0.086, close: 0.099 },
+  { time: "2025-04-07", open: 0.099, high: 0.106, low: 0.094, close: 0.099 },
+  { time: "2025-04-08", open: 0.099, high: 0.106, low: 0.098, close: 0.099 },
+  { time: "2025-04-09", open: 0.099, high: 0.1, low: 0.088, close: 0.099 },
+  { time: "2025-04-10", open: 0.099, high: 0.111, low: 0.088, close: 0.099 },
+  { time: "2025-04-11", open: 0.099, high: 0.107, low: 0.097, close: 0.099 },
+  { time: "2025-04-12", open: 0.099, high: 0.108, low: 0.086, close: 0.099 },
+  { time: "2025-04-13", open: 0.099, high: 0.109, low: 0.091, close: 0.099 },
+  { time: "2025-04-14", open: 0.099, high: 0.112, low: 0.094, close: 0.099 },
+  { time: "2025-04-15", open: 0.099, high: 0.1, low: 0.098, close: 0.099 },
+];
