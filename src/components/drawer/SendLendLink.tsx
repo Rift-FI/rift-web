6c20cf0cac672514a81926bfc7b96c752cf0ec6b
mrgustaveh <mburuantony@outlook.com>
2025-04-19 16:14:57 +0300

    uri encoded redirwect link

--- src/components/drawer/SendLendLink.tsx
+++ src/components/drawer/SendLendLink.tsx
@@ -19,7 +19,9 @@ export const SendLendLink = (): JSX.Element => {
   const onShareTg = () => {
     closeAppDrawer();
     openTelegramLink(
-      `https://t.me/share/url?url=${keyToshare}&text=Use this link to claim the ${secretPurpose} I shared with you on Sphere.`
+      `https://t.me/share/url?url=${encodeURIComponent(
+        keyToshare as string
+      )}&text=Use this link to claim the ${secretPurpose} I shared with you on Sphere.`
     );
   };
 
