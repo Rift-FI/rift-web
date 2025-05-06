b5c0c37ce9d0d817b5e31d4078bb4027ba90d998
mrgustaveh <mburuantony@outlook.com>
2025-05-06 23:58:10 +0300

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
-// import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
   server: { allowedHosts: true },
 });
