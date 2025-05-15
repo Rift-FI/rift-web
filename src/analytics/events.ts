c01b3363456668bb8c70ecb382d1d1977a70d0cd
mrgustaveh <mburuantony@outlook.com>
2025-05-15 16:50:07 +0300

    fixed build error - missing initData

--- src/analytics/events.ts
+++ src/analytics/events.ts
@@ -1,15 +1,26 @@
-import posthog from "posthog-js"
-import { POSTHOG_HOST, POSTHOG_KEY } from "../constants"
+import posthog from "posthog-js";
+import { POSTHOG_HOST, POSTHOG_KEY } from "../constants";
 posthog.init(POSTHOG_KEY, {
-    api_host: POSTHOG_HOST,
-    person_profiles: "identified_only"
-})
-export type ANALYTIC_EVENT_TYPES = "SIGN_UP" | "SIGN_IN" | "APP_LAUNCH" | "COPY_REFFERAL" | "OPEN_INTEGRATION" | "DEPOSIT" | "SEND" | "PAGE_VISIT"
+  api_host: POSTHOG_HOST,
+  person_profiles: "identified_only",
+});
+export type ANALYTIC_EVENT_TYPES =
+  | "SIGN_UP"
+  | "SIGN_IN"
+  | "APP_LAUNCH"
+  | "COPY_REFFERAL"
+  | "OPEN_INTEGRATION"
+  | "DEPOSIT"
+  | "SEND"
+  | "PAGE_VISIT";
 
 type BASE_EVENT_DETAILS = {
-    telegram_id: string
-}
+  telegram_id: string;
+};
 
-export const analyticsLog = (event: ANALYTIC_EVENT_TYPES, data: BASE_EVENT_DETAILS) => {
-    posthog.capture(event, data)
-}
+export const analyticsLog = (
+  event: ANALYTIC_EVENT_TYPES,
+  data: BASE_EVENT_DETAILS
+) => {
+  posthog.capture(event, data);
+};
