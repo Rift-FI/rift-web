96f4a774d2a9b4e6d7c0491fc2047784bb7872f8
porkytheblack <porkytheblack@gmail.com>
2025-06-11 17:46:18 +0300

    Revert "Feat/token details screen (#127)"
    This reverts commit 9868af142f19b86561faa7c59a2add4874c9b1fb.

--- src/v2/pages/home/index.tsx
+++ src/v2/pages/home/index.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@/components/ui/button";
 import SendToKnown from "@/features/send/known";
 
-export default function Home() {
-  return (
-    <div className="w-full h-full flex flex-col items-center justify-center">
-      <p className="font-semibold text-accent-primary">Home</p>
-    </div>
-  );
+
+export default function Home(){
+    return (
+        <div className="w-full h-full flex flex-col items-center justify-center" >
+            <p className="font-semibold text-accent-primary" >
+                Home
+            </p>
+        </div>
+    )
 }
--- src/v2/shell/index.tsx
+++ src/v2/shell/index.tsx
@@ -4,13 +4,13 @@ import PageContainer from "./page-container";
 import Onboarding from "@/features/onboarding";
 import { Routes } from "react-router";
 
+
 export default function AppShell() {
-  return (
-    <ShellContextProvider>
-      {/* INFO: Removed h-screen to allow for full screen height */}
-      <div className="w-screen flex flex-col items-center bg-app-background relative">
-        <PageContainer />
-      </div>
-    </ShellContextProvider>
-  );
+    return (
+        <ShellContextProvider>
+            <div className="w-screen h-screen flex flex-col items-center bg-app-background relative" >
+                <PageContainer />
+            </div>
+        </ShellContextProvider>
+    )
 }
