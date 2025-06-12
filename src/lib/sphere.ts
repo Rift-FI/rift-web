cef95733a65109fa3b3c4d1eaa1486b6b6603f15
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2025-06-12 11:42:33 +0300

    Repair login/signup

--- src/lib/sphere.ts
+++ src/lib/sphere.ts
@@ -2,12 +2,8 @@ import Sphere, { Environment } from "@stratosphere-network/wallet";
 
 const sphere = new Sphere({
   environment: Environment.DEVELOPMENT,
-  apiKey: "sk_4b7b5b5dd8025ec94c1ab4eb79edfd13b41e9d5f6c8504200e0267f4d316a2c3",
-  timeout: 30000, // TODO: move to env file
+  apiKey: "sk_0649f261327a11b68374d6d6b15bbfb0eb039e505d93b6db85f91678eaa374fd",
+  timeout: 30000,
 });
-// TODO: remove; This is just for testing
-sphere.setBearerToken(
-  "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2ODU5N2QxYy1mYzc4LTRhYjAtODYzNC03YzU5OGFiZDZjZTUiLCJlbWFpbCI6Ijc3ODQ4NjMxMTQ4ODg5MDAwIiwiYWRkcmVzcyI6IjB4ZUIwY2ExZUE5YWZjMTk3OWU0YzE1OUQ2Q0ZlNDBCYmUxMWJBMWQwQSIsImJ0Y0FkZHJlc3MiOiJkdW1teWJ0Y2FkZHJlc3MxMjM0NTY3ODkiLCJpYXQiOjE3NDg5MzgzMzZ9.suGG1LELK-rowY_FOH6F9nibBEGbZ_tVo71-LAWJ5qhaGFg2u0yLE7l3gGIA9q-p1pYGvF5slbuw7Ca6yFQnQg"
-);
 
 export default sphere;
